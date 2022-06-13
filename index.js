62a583658a8fb905818c6b7bb8b2f13398a0f502
Douglas Obara <douglasobara97@gmail.com>
2022-06-13 13:39:06 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+  const input = document.getElementById("input");
+  function clickAlert() {
+    alert("Browser input clicked(I was clicked)");
+  }
+  input.addEventListener("click", clickAlert());
 }
+
+// calling the addingEventListener function
+addingEventListener();
